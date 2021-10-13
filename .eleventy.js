697ea19d0ee082fc73faf43bf992ba4a4cdaaab7
Emisod23 <emil.soderlund@elev.ga.ntig.se>
2021-10-13 14:10:44 +0200

    ✨  real stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const moment = require("moment");
+const mtg = require('mtgsdk')
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css/');
     eleventyConfig.addWatchTarget('./src/css/');
+
+    eleventyConfig.addFilter('dateFilter', (value) => {
+        moment.locale('sv');
+        return moment(value).format('LL');
+    })
 
     eleventyConfig.addPlugin(syntaxHighlight);
     return {
@@ -14,14 +21,19 @@ module.exports = function(eleventyConfig) {
     };
 }
 
-const mtg = require('mtgsdk')
+
 
 mtg.card.find(3)
 .then(result => {
-    console.log(result.card.name) // "Black Lotus"
+    console.log(result.card.name)
+    console.log(result.card.rarity)
+    console.log(result.card.set)
+    console.log(result.card.manaCost)
+    console.log(result.card.originalText)
+    console.log(result.card.imageUrl)
 })
 
-mtg.card.all({ name: 'Jace', pageSize: 1 })
-.on('data', card => {
-    console.log(card.name)
+card.where({ name: 'Jace, the Mind Sculptor' })
+.then(cards => {
+    console.log(cards[0].name)
 })
